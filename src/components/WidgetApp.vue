5104660f71d43cf2b8536ef935caf1e5114b23db
Ilya Alikin <iljuha.alikin@gmail.com>
2025-05-29 17:53:26 +0500

    update config

---
+++ src/components/WidgetApp.vue
@@ -0,0 +1,19 @@
+<!-- src/WidgetApp.vue -->
+<script setup>
+import HelloWorld from './components/HelloWorld.vue';
+</script>
+
+<template>
+	<div
+		style="
+			all: initial;
+			font-family: sans-serif;
+			padding: 1em;
+			border: 1px solid #ccc;
+			background: white;
+		"
+	>
+		<h3>Опросник</h3>
+		<HelloWorld msg="Виджет опроса внутри другого сайта" />
+	</div>
+</template>
--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,21 @@
 import { defineConfig } from 'vite';
 import vue from '@vitejs/plugin-vue';
+import { resolve } from 'path';
 
-// https://vite.dev/config/
 export default defineConfig({
 	plugins: [vue()],
+	build: {
+		rollupOptions: {
+			input: {
+				main: resolve(__dirname, 'index.html'),
+				widget: resolve(__dirname, 'src/widget.js'),
+			},
+			output: {
+				entryFileNames: '[name].js',
+			},
+		},
+		outDir: 'dist',
+		emptyOutDir: true,
+		target: 'es2015',
+	},
 });
